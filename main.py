c90e195d17eabac6a389deb99b89e156e2ed44ca
GolasalaPuneeth <puneeth3sprime@gmail.com>
2026-04-23 20:39:34 +0530

    #23042026 update

--- main.py
+++ main.py
@@ -1,1 +1,60 @@
-print("hello123")
+def sample_1(a:int,b:int) -> int:
+    """
+    This function will gives u sum of 2 numbers
+    a : should be int
+    b : should be int
+
+    return : int
+
+    """
+    print("in Function - ",a+b)
+    return a+b
+
+# =============================================
+
+def greet(name):
+    print(f"Hi {name} how are you")
+    return f"Hi {name} how are you"
+# =================================================
+
+def area_square(s:int)->float:
+    return(float(s**2))
+# ===================================================
+
+def  greet_mult(*names):
+    for i in names:
+        greet(i)
+# =====================================================
+
+def greet_everyone(time_ist: int,name="Everyone"):
+    print(f"Hi {name} How are you {time_ist}")
+# ======================================================
+
+#    count = 0
+#     while(count<10):
+#         # count+=1
+#         print(count)
+#         count = count + 1
+
+# =======================================================
+def main():
+    count = 0
+    while 1:
+        count+=1
+        if count==10:
+            continue
+        print(count)
+
+
+
+
+
+
+    
+
+    
+   
+
+
+if __name__=="__main__":
+    main()
